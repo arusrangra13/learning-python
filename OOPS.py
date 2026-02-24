694d088a6b72b5d87d227edb630f452bc6916c7d
Arus Rangra <arushrangra@gmail.com>
2026-02-24 21:01:32 +0530

    Update OOPS.py

--- OOPS.py
+++ OOPS.py
@@ -4,3 +4,11 @@ class Student:
 
 s1 = Student()
 print(s1.name)
+
+# using function 
+class Student:
+    def __init__(self,fullname):
+        self.name=fullname
+ 
+s1 = Student("karan")
+print(s1.name)
