be60b647c2b90d29a3527bdf1a6f161988926a2f
Arus Rangra <arushrangra@gmail.com>
2026-02-28 15:30:29 +0530

    Create Update a class.py

---
+++ Update a class.py
@@ -0,0 +1,17 @@
+# Updating car speed
+class Car:   # class names should start with Capital letter
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+
+    def update_speed(self, speed):
+        self.speed = speed
+
+    def show(self):
+        print(self.brand, self.speed)
+
+c1 = Car("BMW", 200)
+c1.show()
+
+c1.update_speed(250)
+c1.show()
