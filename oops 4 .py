c356a83f24d32fd3d85927db5d155867091b412a
Arus Rangra <arushrangra@gmail.com>
2026-03-07 19:53:27 +0530

    Implement car class with start method

---
+++ oops 4 .py
@@ -0,0 +1,17 @@
+class car:
+    def start(self):
+        self.acc=False
+        self.brk=False
+        self.clutch=False
+
+    def start(self):
+        self.acc=False
+        self.brk=True
+        self.clutch=True
+        print("car started..")
+
+car1=car()
+car1.start()
+
+car2=car()
+car2.start()
