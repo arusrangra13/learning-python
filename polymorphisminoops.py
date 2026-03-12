8991de6960ed2b98d1317bc7c627f332b278f95f
Arus Rangra <arushrangra@gmail.com>
2026-03-12 22:29:07 +0530

    Add Complex class for complex number operations
    Implement Complex class with addition and display methods.

---
+++ polymorphisminoops.py
@@ -0,0 +1,23 @@
+class Complex:
+    def __init__(self,real,img):
+        self.real = real
+        self.img = img
+
+    def ShowNumbers(self):
+        print(self.real,"i+",self.img,"j")
+
+    def add(self,num2):
+        newReal = self.real +num2.real
+        newImg = self.img +num2.img
+        return Complex(newReal,newImg)
+
+
+
+num1 = Complex(1,3)
+num1.ShowNumbers()
+
+num2 = Complex(3,5)
+num2.ShowNumbers()
+
+num3 = num1.add(num2)
+num3.ShowNumbers()
